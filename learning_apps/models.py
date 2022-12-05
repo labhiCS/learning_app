6d8d217450460458dd34cde87417d0343d6db12e
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-05 20:16:29 +0545

    Djanjo project

--- learning_apps/models.py
+++ learning_apps/models.py
@@ -9,3 +9,14 @@ class Topic(models.Model):
     def _str_(self):
         """Return string stored in text argument."""
         return self.text
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    Date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def _str_(self):
+        return self.text [ :50] + "..."
