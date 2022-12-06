ae0bc05404d8beffbf403f621fdbd579eba21ed8
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-06 21:50:36 +0545

    Home Page

---
+++ learning_apps/urls.py
@@ -0,0 +1,9 @@
+"""Defines URL patterns for learning_apps."""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
--- learning_apps/views.py
+++ learning_apps/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The home page for Learning app"""
+    return render(request, 'learning_apps/index.html')
