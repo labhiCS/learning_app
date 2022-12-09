3b23b60dbee589bf46603a2cc8cf4dd4b5d59065
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-09 19:36:48 +0545

    corrected

--- learning_apps/urls.py
+++ learning_apps/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page.
     path('', views.index, name='index'),
+    #show all Topics.
+    path('topic/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
