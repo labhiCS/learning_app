723d60ba85d6afa763f4ed98bcd520eb43a4557d
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-09 20:06:01 +0545

    corrected

--- learning_apps/views.py
+++ learning_apps/views.py
@@ -18,4 +18,4 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+    return render(request, 'learning_apps/topic.html', context)
