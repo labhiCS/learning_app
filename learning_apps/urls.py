80e9054bce5e42086b688fcab29b9e9728159473
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-10 21:28:40 +0545

    initial commit

--- learning_apps/urls.py
+++ learning_apps/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'learning_apps'
 urlpatterns = [
     # Home page.
     path('', views.index, name='index'),
     #show all Topics.
-    path('topic/', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
     # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
