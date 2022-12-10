d91af6250422c4822e10b54dd54bc9654c2bdf89
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-10 21:26:19 +0545

    initial commit

--- learning_app/settings.py
+++ learning_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'learning_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates/index.html')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
