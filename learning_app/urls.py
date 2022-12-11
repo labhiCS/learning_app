136b384444d10bc32b26dfaedd20f94fe0a3da0e
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-11 20:20:06 +0545

    test

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('learning_logs.urls', namespace='learning_apps')),
+    url(r'', include('learning_logs.urls', namespace='learning_s')),
 ]
