91a30f09ad28a9ebe7b85e995dd9caa0496e6ee4
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-11 20:17:15 +0545

    corrected

--- learning_apps/urls.py
+++ learning_apps/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Show all topics.
     url(r'^topics/$', views.topics, name='topics'),
+     # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
