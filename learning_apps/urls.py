7d991872184dfbeefadb09dd78a2f0d1dd55ca58
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-11 21:20:12 +0545

    Corrected

--- learning_apps/urls.py
+++ learning_apps/urls.py
@@ -1,14 +1,18 @@
 """Defines URL patterns for learning_apps."""
 
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+#from django.conf.urls import url
+
+app_name = 'learning_apps'
 urlpatterns = [
-    # Home page
-    url(r'^$', views.index, name='index'),
-    # Show all topics.
-    url(r'^topics/$', views.topics, name='topics'),
-     # Detail page for a single topic
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # Home page.
+    path('', views.index, name='index'),
+    #url(r'^topics/$', views.topics, name='topics'),  #(not working in old version.)
+     # Show all topics.
+    path('topics/', views.topics, name='topics'),
+     # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
